deee901759d380722f28be23b2c5e8daf3c72fde
Adrià Lorente <adria810@gmail.com>
2025-07-10 13:32:00 +0200

    feat: add Month class with name attribute and constructor

---
+++ src/ex1/Month.java
@@ -0,0 +1,13 @@
+package ex1;
+
+public class Month {
+    private String name;
+
+    public Month(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
