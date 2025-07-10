a4f92f9eefaff740e22479061858e187bb3bb6e0
Adrià Lorente <adria810@gmail.com>
2025-07-10 18:06:06 +0200

    feat(main): print lists with loop and iterator

--- src/ex1/Main.java
+++ src/ex1/Main.java
@@ -1,26 +1,40 @@
 package ex1;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Month> months = new ArrayList<>();
+        ArrayList<Month> listMonths = new ArrayList<>();
 
-        months.add(new Month("January"));
-        months.add(new Month("February"));
-        months.add(new Month("March"));
-        months.add(new Month("April"));
-        months.add(new Month("May"));
-        months.add(new Month("June"));
-        months.add(new Month("July"));
-        months.add(new Month("September"));
-        months.add(new Month("October"));
-        months.add(new Month("November"));
-        months.add(new Month("December"));
-        System.out.println(months);
+        listMonths.add(new Month("January"));
+        listMonths.add(new Month("February"));
+        listMonths.add(new Month("March"));
+        listMonths.add(new Month("April"));
+        listMonths.add(new Month("May"));
+        listMonths.add(new Month("June"));
+        listMonths.add(new Month("July"));
+        listMonths.add(new Month("September"));
+        listMonths.add(new Month("October"));
+        listMonths.add(new Month("November"));
+        listMonths.add(new Month("December"));
 
-        months.add(7, new Month("August"));
-        System.out.println(months);
+        // Using for to print the list before adding August
+        System.out.println("\n--- Month list before adding August ---");
+        for (int i = 0 ; i < listMonths.size() ; i++) {
+            System.out.println((i + 1) + ". " + listMonths.get(i).getName());
+        }
+
+        listMonths.add(7, new Month("August"));
+
+        // Using an Iterator to print the list after adding August
+        System.out.println("\n--- Month list after adding August ---");
+        Iterator<Month> it = listMonths.iterator();
+        int i = 1;
+        while (it.hasNext()) {
+            System.out.println(i + ". " + it.next().getName());
+            i++;
+        }
 
     }
 }
